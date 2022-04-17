6e7f62055bdb0dbcc5fdcc1104575c99641d8f14
nizkiyVorobey <formobileprima@gmail.com>
2022-04-17 17:28:01 +0300

    add localBroadcastReceiver

--- app/src/main/java/com/example/broadcast/MainActivity.kt
+++ app/src/main/java/com/example/broadcast/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ProgressBar
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.broadcast.MyReceiver.Companion.EXTRA_CLICKED_COUNT
 
 class MainActivity : AppCompatActivity() {
@@ -16,11 +17,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var progressBar: ProgressBar
 
+    private val localBroadcastReceiver by lazy {
+        LocalBroadcastManager.getInstance(this)
+    }
+
     /**
      * Коли нам треба в змінювати layout в залежності від даних з BroadcastReceiver, ми
      * створюємо анонімний клас, там де будемо змунювати layout
+     *
+     *
+     * Зараз коли ми стоврюжмо якийсь BroadcastReceiver то будь який додаток зможе на нього підписатися,
+     * це не дуже безпечно, якщо ми хочемо передати якісь персональні дані юзера. Якщо ми хочемо передавати лише
+     * всередені нашої апки то трба використовувати LocalBroadcastManager
      */
-    private val receiver = object :BroadcastReceiver() {
+    private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             if (intent?.action == "loaded") {
                 val percent = intent.getIntExtra("percent", 0)
@@ -41,12 +51,14 @@ class MainActivity : AppCompatActivity() {
 
         // intentFiler потрібен, щоб нащ receiver реагував не на всі події, а тільки на ті, які нам потрібні
         val intentFiler = IntentFilter().apply {
-            addAction(Intent.ACTION_BATTERY_LOW)
-            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-            addAction(MyReceiver.ACTION_CLICKED)
-            addAction("loaded")
+            with(localBroadcastReceiver) {
+                addAction(Intent.ACTION_BATTERY_LOW)
+                addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+                addAction(MyReceiver.ACTION_CLICKED)
+                addAction("loaded")
+            }
         }
-        registerReceiver(receiver, intentFiler)
+        localBroadcastReceiver.registerReceiver(receiver, intentFiler)
 
         findViewById<Button>(R.id.button).setOnClickListener {
             val intent = Intent(MyReceiver.ACTION_CLICKED).apply {
@@ -64,6 +76,6 @@ class MainActivity : AppCompatActivity() {
     // Якщо ми підпислися на Receiver, то ми маємо і відписатися від нього, щоб не було відтоку пам'яті
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(receiver)
+        localBroadcastReceiver.unregisterReceiver(receiver)
     }
 }
--- app/src/main/java/com/example/broadcast/MyService.kt
+++ app/src/main/java/com/example/broadcast/MyService.kt
@@ -3,9 +3,14 @@ package com.example.broadcast
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import kotlin.concurrent.thread
 
 class MyService : Service() {
+
+    private val localBroadcastReceiver by lazy {
+        LocalBroadcastManager.getInstance(this)
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         thread {
@@ -14,7 +19,7 @@ class MyService : Service() {
 
                 Intent("loaded").apply {
                     putExtra("percent", i * 10)
-                    sendBroadcast(this) // Кожну секунду буду надсилатися Broadcast з відсотками, 10, 20, 30 ...
+                    localBroadcastReceiver.sendBroadcast(this) // Кожну секунду буду надсилатися Broadcast з відсотками, 10, 20, 30 ...
                 }
             }
         }
